bc96e43ad253dc631a2333a426c816f695a25f41
KTajerbashi <kamrantajerbashi@gmail.com>
2024-08-27 23:07:09 +0330

    Feature : Add Backend Application

---
+++ CRM/CRM.EndPoint.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRM.EndPoint.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
