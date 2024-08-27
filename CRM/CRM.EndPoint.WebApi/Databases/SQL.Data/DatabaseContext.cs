96fd2d015b05cbe95404279c71e9bb50e9b2bc60
KTajerbashi <kamrantajerbashi@gmail.com>
2024-08-28 00:56:04 +0330

    Feature : Add Database

---
+++ CRM/CRM.EndPoint.WebApi/Databases/SQL.Data/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using CRM.EndPoint.WebApi.ApplicationBase.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.EndPoint.WebApi.Databases.SQL.Data;
+
+public class DatabaseContext : BaseDatabaseContext
+{
+    protected DatabaseContext()
+    {
+
+    }
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.AddBuilderConfiguration();
+    }
+}
---
+++ CRM/CRM.EndPoint.WebApi/Databases/SQL.Data/Extensions/DatabaseExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRM.EndPoint.WebApi.Databases.SQL.Data.Extensions;
+
+public static class DatabaseExtentions
+{
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<DatabaseContext>(option =>
+        {
+            option.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+        return services;
+    }
+}
