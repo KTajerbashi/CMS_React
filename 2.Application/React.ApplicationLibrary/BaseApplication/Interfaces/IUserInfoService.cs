dc44cc7f08a23f17539482705dff79ab27a23d79
Tajerbashi <kamrantajerbashi@gmail.com>
2025-01-03 15:29:47 +0330

    Feature : Add Application Layer

---
+++ 2.Application/React.ApplicationLibrary/BaseApplication/Interfaces/IUserInfoService.cs
@@ -0,0 +1,8 @@
+﻿namespace React.ApplicationLibrary.BaseApplication.Interfaces;
+
+public interface IUserInfoService
+{
+    string UserIp { get; }
+    string UserAgent { get; }
+    int UserId { get; }
+}
---
+++ 2.Application/React.ApplicationLibrary/DepenedencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace React.ApplicationLibrary;
+
+public static class DepenedencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        return services;
+    }
+}
